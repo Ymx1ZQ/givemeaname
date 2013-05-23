7926cb04d1518966d992f4e886691ca0146dcc98
alpkeser <yalpkeser@gmail.com>
2013-05-22 22:06:39 -0700

    about us

--- application/views/static_pages/about.php
+++ application/views/static_pages/about.php
@@ -14,7 +14,63 @@ $this->view("navBar.php");
   <div class="container">
 
       <h1>About Us</h1>
-      <p>Use this document as a way to quick start any new project.<br> All you get is this message and a barebones HTML document.</p>
+      <p>Many children in underdeveloped or developing countries don’t have a legal name, so as they 
+
+
+
+are not registered in the civil registry, their existence is not legally recognized.
+
+
+
+At GiveMeAName, we want to change this situation creating a crowd-funding platform for 
+
+
+
+long-distance adoption to give these children shelter, food, education and a legal name. With 
+
+
+
+your help, we will be able to save a lot of lives.
+
+
+
+For $5, GiveMeAName gives you the opportunity to propose a name for one of these children 
+
+
+
+and explain why you want that name to be given. People who like this name can vote for it and 
+
+
+
+donate so that one child can receive a legal name. This would be the child’s second name to 
+
+
+
+reduce cultural issues and promote multi-cultural integration. 
+
+
+
+When one name gathers $6000, which is the amount of money necessary to raise and educate 
+
+
+
+a child from childhood to maturity, a child is actually adopted. An annual amount of money 
+
+
+
+will be given to a no-profit organization to take care of the child and register him/her with the 
+
+
+
+chosen name. 
+
+
+
+Since the adoption, GiveMeAName creates a connection between donors and children, 
+
+
+
+including in our web pictures, blog posts and videos of every adopted child.</p>
 
     </div> <!-- /container -->
 
