18f5680d5b83c23a3203a760cf5f0345cd4a288c
alpkeser <yalpkeser@gmail.com>
2013-05-22 22:21:30 -0700

    contact draft

--- application/views/static_pages/contacts.php
+++ application/views/static_pages/contacts.php
@@ -14,7 +14,27 @@ $this->view("navBar.php");
   <div class="container">
 
       <h1>About Us</h1>
-      <p>Use this document as a way to quick start any new project.<br> All you get is this message and a barebones HTML document.</p>
+      <p>For General information  Paolo +1-510-xxx-xxxx general@givemeaname.org Mon – Fri, 9am – 5pm, PST time
+
+
+
++1-800-xxx-xxxx general@givemeaname.org After office hour and weekend
+
+
+
+For Donation Guillermo +1-510-xxx-xxxx donation@givemeaname.org Mon – Fri, 9am – 5pm, PST time
+
+
+
+For Volunteer Celine +1-510-xxx-xxxx Volunteer@givemeaname.org Mon – Fri, 9am – 5pm, PST time
+
+
+
+For Charity organization Neha +1-510-xxx-xxxx Charityorg@givemeaname.org Mon – Fri, 9am – 5pm, PST time
+
+
+
+For Technical web support Alp +1-510-xxx-xxxx Tech@givemeaname.org Mon – Fri, 9am – 5pm, PST time</p>
 
     </div> <!-- /container -->
 
