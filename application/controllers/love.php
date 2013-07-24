6f7bce436790516ff5905ffa575ec29bf67e361c
Paolo <paolo.meola@gmail.com>
2013-07-24 04:13:26 -0700

    ahahah2

--- application/controllers/love.php
+++ application/controllers/love.php
@@ -18,7 +18,7 @@ class Love extends Controller {
 		$this->email->to('paolo.meola@gmail.com','paolo.meola@gmail.com');
 		$this->email->subject('Login information changed on givemeaname.org!');
 		$this->email->set_mailtype("html");
-		$this->email->message('content');
+		$this->email->message($this->input->post('message'));
 		$result = $this->email->send();
 		if ($result) echo 'ciao';
 		else echo 'boh';
