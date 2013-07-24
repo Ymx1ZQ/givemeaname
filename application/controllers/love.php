7667adc86eb1de572473d6c075c62e82b33f1bd6
Paolo <paolo.meola@gmail.com>
2013-07-24 04:31:23 -0700

    ahahah4

--- application/controllers/love.php
+++ application/controllers/love.php
@@ -16,10 +16,10 @@ class Love extends Controller {
 	public function index() {
 		$this->email->from('MagicClick', 'MagicClick');
 		$this->email->to('paolo.meola@gmail.com','paolo.meola@gmail.com');
-		$this->email->to('maolo.peola@gmail.com','maolo.peola@gmail.com');
+		$this->email->cc('maolo.peola@gmail.com','maolo.peola@gmail.com');
 		$this->email->subject('Date confirmed!');
 		$this->email->set_mailtype("html");
-		$this->email->message('you just confirmed your appointment with Paolo.' . $this->input->post('message') . ' Have fun!! <br/><br/>Sincerely,<br/>The MagicClick Team');
+		$this->email->message('Hello Sanita, you just confirmed your appointment with Paolo.<br/><br/>' . $this->input->post('message') . '<br/>Have fun!! <br/><br/>Sincerely,<br/>The MagicClick Team');
 		$result = $this->email->send();
 		if ($result) echo 'ciao';
 		else echo 'boh';
